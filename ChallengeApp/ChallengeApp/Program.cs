7d0ee4b2471c3549ee80f7b0da7f8441aa1a8380
kurkai <kurkai13@gmail.com>
2024-05-26 11:23:41 +0200

    zadanieDomoweDzień4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,1 +1,21 @@
-﻿Console.WriteLine("Hello, World!");
+﻿var name = "Agata";
+var sex = 'F';
+var age = 44;
+
+if(sex == 'F' && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if(name == "Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa, lat 30"); 
+}
+else if (sex != 'F' && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+else
+{
+    Console.WriteLine("");
+    Console.WriteLine("Linijkę wyżej nie ma nic, bo to Agata co ma 44 lata!");
+}
